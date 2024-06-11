5531eade248f3d5011bab06cd14837c514a67eac
Mohan Paranthaman <mohanhuzzain@gmail.com>
2024-06-11 15:01:38 +0530

    added the source file to gitup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+function Car(props){
+  return (
+    <h1>My name is {props.brand}</h1>
+  )
+}
+
+
+function Garage(){
+  return (
+    <>
+    <h1>Who lives in Garage</h1>
+    <Car brand='Audi'/>
+    </>
+  )
+}
+
+export default Garage;
+
